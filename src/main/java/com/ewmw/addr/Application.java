252a72f73f64b27708edaaebef0777f7ba919027
silenseram <ew.mwriter@gmail.com>
2022-08-11 10:49:16 +0300

    cleanup

--- src/main/java/com/ewmw/addr/Application.java
+++ src/main/java/com/ewmw/addr/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
